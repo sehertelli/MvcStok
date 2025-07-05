99ef1f4803add06d1501875f7829029b59f5c0b7
Seher Telli <seherrtll@gmail.com>
2025-07-05 23:38:45 +0300

    MvcStok

---
+++ MvcStok/MvcStok/Controllers/SatisController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult SatisListe()
+        {
+            var degerler = db.TBLSATISLAR.ToList();
+            return PartialView(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(TBLSATISLAR p)
+        {
+            db.TBLSATISLAR.Add(p);
+            db.SaveChanges();
+            return View("Index");
+        }
+    }
+}
